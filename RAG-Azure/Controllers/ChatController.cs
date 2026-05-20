eaafc21eb81e1c527471cc55df051845ba337f53
KaveendraY <pasindukaveendra123@gmail.com>
2026-05-20 14:57:05 +0530

    service implementation

---
+++ RAG-Azure/Controllers/ChatController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Runtime.CompilerServices;
+using RAG.API.Services;S
+
+namespace RAG_Azure.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChatController : Controller
+    {
+        private readonly AzureSearchService _searchService;
+        private readonly AzureAIService _aiService;
+
+        public ChatController(AzureSearchService searchService, AzureAIService aiService)
+        {
+            _searchService = searchService;
+            _aiService = aiService;
+        }
+
+        [HttpPost]
+
+        public async Task<IActionResult> Post([FromBody] ChatRequest request)
+        {
+            if(string.IsNullOrWhiteSpace(request.Message))
+                return BadRequest("Message cannot be empty.");
+            var relevantChunks = await _searchService.SearchRelevantChunks(request.Message);
+            var context = string.Join("\n", relevantChunks);
+            var answer = await _aiService.AskQuestionAsync(request.Message, context);
+
+            return Ok(new {question = request.Message, answer });
+        }
+
+        public IActionResult Index()
+        {
+    
+
+            return View();
+        }
+    }
+}
---
+++ RAG-Azure/Controllers/FileUploadController.cs
@@ -0,0 +1,29 @@
+﻿using Azure.Storage.Blobs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RAG_Azure.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FileUploadController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+        public FileUploadController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UploadFile(IFormFile file)
+        {
+            var blobClient = new BlobContainerClient(_config["AzureBlobStorage:ConnectionString"], _config["AzureBlobStorage:ContainerName"]);
+            await blobClient.CreateIfNotExistsAsync();
+
+            var blob = blobClient.GetBlobClient(file.FileName);
+            await using var stream = file.OpenReadStream();
+            await blob.UploadAsync(stream, overwrite: true);
+
+            return Ok(new { file.FileName });
+        }
+    }
+}
